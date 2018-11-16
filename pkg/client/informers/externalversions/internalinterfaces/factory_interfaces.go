f78b7a4472c32947eb31a5546f9f6f08d0a9f26b
Petr Horacek <phoracek@redhat.com>
2018-11-16 14:43:37 +0100

    use all lower-case letters for imports
    Using upper-case letters in organization name breaks
    kubevirt/code-generator <= 1.13. In order to prevent that, this client
    must be always used with lower-letters: k8snetworkplumbingwg.

--- pkg/client/informers/externalversions/internalinterfaces/factory_interfaces.go
+++ pkg/client/informers/externalversions/internalinterfaces/factory_interfaces.go
@@ -21,7 +21,7 @@ package internalinterfaces
 import (
 	time "time"
 
-	versioned "github.com/K8sNetworkPlumbingWG/network-attachment-definition-client/pkg/client/clientset/versioned"
+	versioned "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/clientset/versioned"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	cache "k8s.io/client-go/tools/cache"
