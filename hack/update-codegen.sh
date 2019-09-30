381919f47119aad652f6c5be159f834cd7d3a610
Tomofumi Hayashi <tohayash@redhat.com>
2019-09-30 19:04:39 +0900

    Go module support

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-vendor/k8s.io/code-generator/generate-groups.sh all \
-  github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis \
+bash vendor/k8s.io/code-generator/generate-groups.sh all \
+  github.com/K8sNetworkPlumbingWG/network-attachment-definition-client/pkg/client github.com/K8sNetworkPlumbingWG/network-attachment-definition-client/pkg/apis \
   k8s.cni.cncf.io:v1 \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -19,7 +19,7 @@ limitations under the License.
 package versioned
 
 import (
-	k8scnicncfiov1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/clientset/versioned/typed/k8s.cni.cncf.io/v1"
+	k8scnicncfiov1 "github.com/K8sNetworkPlumbingWG/network-attachment-definition-client/pkg/client/clientset/versioned/typed/k8s.cni.cncf.io/v1"
 	discovery "k8s.io/client-go/discovery"
 	rest "k8s.io/client-go/rest"
 	flowcontrol "k8s.io/client-go/util/flowcontrol"
